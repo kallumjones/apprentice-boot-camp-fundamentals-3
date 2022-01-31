ecf118212e27841612fe5338474bc889bb610c4b
Kallum Jones <kallum.jones@autotrader.co.uk>
2022-01-31 13:42:04 +0000

    Add petrol price code to make test

--- exercises/javascript/tax-calculator.js
+++ exercises/javascript/tax-calculator.js
@@ -13,8 +13,37 @@ let TaxCalculator = class TaxCalculator {
   }
 
   calculateTax(vehicle) {
-    throw new Error('You have to implement the method doSomething!');
+    let taxPrice;
+    const {co2Emissions, fuelType} = vehicle;
+
+    const petrolPrice ={
+      0: 0,
+      50: 10,
+      75: 25,
+      90: 105,
+      100: 125,
+      110: 145,
+      130: 165,
+      150: 205,
+      170: 515,
+      190: 830,
+      225: 1240,
+      255: 1760,
+      100000: 2070
+    }
+
+    const vehicleCo2 = Object.keys(petrolPrice).find(element => co2Emissions <= element);
+    taxPrice = petrolPrice[vehicleCo2];
+
+    return taxPrice;
+    // throw new Error('You have to implement the method doSomething!');
   }
+
+
 }
+
+// let taxCalculatorTest = new TaxCalculator(2019);
+// const vehicleTest = new Vehicle(50, FuelType.PETROL, FIRST_OF_JAN_2020, 20000);
+
 
 module.exports = { TaxCalculator: TaxCalculator }
--- exercises/javascript/test/tax-calculator-petrol.test.js
+++ exercises/javascript/test/tax-calculator-petrol.test.js
@@ -1,13 +1,15 @@
 const { DummyTaxCalculator } = require('./dummy-tax-calculator');
+const { TaxCalculator } = require('../tax-calculator')
 const { Vehicle } = require('../vehicle');
 const { FuelType } = require('../fuel-type');
 
 describe('Tax calculator on a petrol vehicle for the first year', () => {
-  let taxCalculator = new DummyTaxCalculator();
+  let taxCalculator = new TaxCalculator(2019);
   let FIRST_OF_JAN_2020 = new Date(2020, 1, 1);
 
   it('should return zero for zero grams of CO2 emissions', () => {
     const vehicle = new Vehicle(0, FuelType.PETROL, FIRST_OF_JAN_2020, 20000);
+    taxCalculator.calculateTax(vehicle);
     expect(taxCalculator.calculateTax(vehicle)).toBe(0);
   })
 
